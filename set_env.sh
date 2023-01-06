205e1ee08082bd53f0defa82293b12838ec85407
ZITA0123 <fontsingzita@gmail.com>
2023-01-06 19:17:57 +0100

    virtualization project

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=admin
+export POSTGRES_PASSWORD=admin
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=dbesiea
+export AWS_BUCKET=udagram-esiea-zi
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
